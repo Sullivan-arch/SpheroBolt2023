943ba6b47ea21991aa952ea03faddcc98e7e53c0
Sullivan A <sullivanabegg8@gmail.com>
2023-02-10 08:31:19 -0500

    Added speedFunction

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -3,11 +3,10 @@
 //Project: Sphero Bolt
 
 
-/**
- * Hello World Practice Program
- * LED turn to blue
- * Forward for for 2 seconds at a speed of 60
-
+/**Hello World Practice Program
+ *LED turn to blue
+ *Forward for for 2 seconds at a speed of 60
+*/
  async function helloWorld() {
  await speak("Hello World", true);
  setMainLed({ r: 0, g: 0, b: 255});
@@ -18,29 +17,30 @@
 }
 
 
-/**
- * ninetyDegree function goes forward for a specified amount of time and speed
- * pausing, turning 90degrees clockwise then moving forward for a specific amount
- * of speed and time
 
-async function ninetyDegree() {
-    await roll((getHeading() + 0), 60, 4); // Heading 0 degrees at 60 speed for 4 seconds
-    await delay(1);
-   await roll((getHeading() + 90), 60, 4); // heading 90 degrees at 60 speed for 4 seconds
-}
+/**fullSpeed function goes forward for 6 seconds at 100 speed
 */
+async function fullSpeed() {
+    await roll((getHeading() + 0), 100, 6); // Heading 0 degrees at 60 speed for 4 seconds
+    await delay(1);
+    await Sound.Animal.Rooster.play(true);
+   await roll((getHeading() + 270), 250, 6); // Heading 0 degrees at 60 speed for 4 seconds
+    await Sound.Animal.Rooster.play(true);
+    await roll((getHeading() + 270), 500, 6); // Heading 0 degrees at 60 speed for 4 seconds
+}
+
 /**
  * startSquare function turns to hte color blue at high brightness. It then says Hello Square waits one second and then spin 360 degrees
  * It then runs a loop 4 times. It changes it's color randomly plays a coin sound and then rotates 90 degrees and moves forward at 60 speed
  * for 3 seconds
 */
-async function startSquare() {
+/**async function startSquare() {
 
-    setMainLed({ r: 0, g: 0, b: 255 });
+    *setMainLed({ r: 0, g: 0, b: 255 });
 
-    await speak("Hello Square", true);
+    *await speak("Hello Square", true);
 
-    await delay(1);
+   8 await delay(1);
     
     await spin(360, 1)
 
@@ -54,15 +54,23 @@ async function startSquare() {
 
         await delay(1);
 
-    }
+   }
 
 }
+
+
+
+
+
+
+
+
 
 /**
  * startProgram function sends the program to your Sphero Bolt
 */
 async function startProgram() {
-    await startSquare()  //<--- calling hello world function
+    await fullSpeed()  //<--- calling hello world function
 /**await ninetyDegree()   //<--calling the ninetyDegree function to run it
 */
 }
