68c2bac88ca21f1ef4f7b541aa5f0c43e49eb627
Sullivan A <sullivanabegg8@gmail.com>
2023-02-13 08:36:03 -0500

    True Blue (Other is false)

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -7,18 +7,19 @@
  *Forward for for 2 seconds at a speed of 60
 */
  async function ledSpeak() {
- await scrollMatrixText('Start', { r: 0, g: 0, b: 0 }, 30, true)
+ await scrollMatrixText('Start', { r: 225, g: 225, b: 225 }, 30, true)
  setSpeed(60);
  await delay (2);
  setSpeed(0);
 
 }
 async function ninetyDegrees() {
-    await roll((getHeading() + 0), 40, 2);
+    await roll((getHeading() + 0), 30, 2);
     setMainLed({ r: 0, g: 0, b: 255});
     await delay (2);
     await roll((getHeading() + 90), 40, 2);  
-    
+    await delay (2);
+    await roll((getHeading() + 90), 40, 2);
 }
 
 async function startProgram() {
