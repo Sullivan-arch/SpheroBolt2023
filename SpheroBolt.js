9589fd4314b3976a55ec5edb14f1fffd10fab744
Sullivan A <sullivanabegg8@gmail.com>
2023-02-20 08:38:01 -0500

    Green working

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -18,30 +18,33 @@ async function runMaze() {
     setMainLed({ r: 0, g: 0, b: 255});
     await delay (2);
     
-    await roll((getHeading() + 90), 52, 2);  
+    await roll((getHeading() + 90), 50, 2);  
     await delay (2);
     await roll((getHeading() + 90), 39, 2);
     await Sound.Animal.Eagle.play(true);
+    await delay (2);
     
     await roll((getHeading() + 35), 38, 2);
     setMainLed({ r: 225, g: 0, b: 0});
     await delay (2);
     
     await roll((getHeading() + 270), 26, 2);
-    await roll((getHeading() + 270), 21, 2);
-    await roll((getHeading() + 0), 25, 2);
-    await roll((getHeading() + 90), 25, 2);
+    await roll((getHeading() + 270), 23, 2);
+    await roll((getHeading() + 0), 23, 2);
+    await delay (2);
+    await roll((getHeading() + 90), 27, 2);
     await Sound.Animal.Eagle.play(true);
+    await delay (2);
     
-    await roll((getHeading() + 45), 32, 2);
-    await roll((getHeading() + 270), 32, 2);
+    await roll((getHeading() + 45), 30, 2);
+    await roll((getHeading() + 270), 30, 2);
     await delay (2);
     setMainLed({ r: 0, g: 225, b: 0});
     
     await roll((getHeading() + 270), 30, 2);
     await roll((getHeading() + 315), 25, 1);
-    await roll((getHeading() + 90), 90, 1);
-    setMainLed({ r: 40, g: 0, b: 225});
+    await roll((getHeading() + 270), 45, 1);
+    
 }
 
 async function startProgram() {
